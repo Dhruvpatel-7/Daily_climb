ae9736311c761de28ba1a98e0a7a9a8eebc97d62
Dhruv Patel <dnp1982002@gmail.com>
2024-09-18 16:43:21 +0530

    Update patern.ts

--- daily_Programs/patern.ts
+++ daily_Programs/patern.ts
@@ -19,3 +19,14 @@ function triangle(rows:number){
     }
 }
 triangle(4)
+
+function numbertriangle(rows:number){
+    for(let i= 1; i <= rows; i++){
+        let line = "";
+        for(let j =1; j <= i; j++){
+            line += i;
+        }
+        console.log(line)
+    }
+}
+numbertriangle(9)
